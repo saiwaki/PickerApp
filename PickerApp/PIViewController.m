8d3b281cfee3fc22b372d0d3f5054519c8fcae6d
Hitoshi Saiwaki <saiwaki@undapt.jp>
2013-02-27 08:19:07 +0900

    first commit

--- PickerApp/PIViewController.m
+++ PickerApp/PIViewController.m
@@ -18,7 +18,36 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSDictionary *dic = [NSDictionary dictionaryWithObject:@"KHQvcu4yb2bObgdLrI3SSngsUBKUJpmLwu0cCBC1KJg=" forKey:@"authenticity_token"];
+    NSData *data = [self encodeDictionary:dic];
+    
+    
+    NSURL *url = [[NSURL alloc]initWithString:@"http://localhost:3000/tweets"];
+    NSMutableURLRequest *req = [[NSMutableURLRequest alloc]initWithURL:url];
+    [req setHTTPMethod:@"POST"];
+    [req setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-type"];
+    [req setValue:[NSString stringWithFormat:@"%d",[data length]] forHTTPHeaderField:@"Content-Length"];
+    [req setHTTPBody:data];
+    [req setHTTPShouldHandleCookies:YES];
+    NSURLConnection *con = [[NSURLConnection alloc]initWithRequest:req delegate:self];
+    if (con) {
+        
+    }
+    
 }
+
+- (NSData*)encodeDictionary:(NSDictionary*)dictionary {
+    NSMutableArray *parts = [[NSMutableArray alloc] init];
+    for (NSString *key in dictionary) {
+        NSString *encodedValue = [[dictionary objectForKey:key] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        NSString *encodedKey = [key stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        NSString *part = [NSString stringWithFormat: @"%@=%@", encodedKey, encodedValue];
+        [parts addObject:part];
+    }
+    NSString *encodedDictionary = [parts componentsJoinedByString:@"&"];
+    return [encodedDictionary dataUsingEncoding:NSUTF8StringEncoding];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
